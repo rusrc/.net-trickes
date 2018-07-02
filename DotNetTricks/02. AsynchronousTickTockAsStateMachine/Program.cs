aaf5965766ecc6e8510307aa072715dcf646cc97
rusrc <ruslan878707@gmail.com>
2018-07-03 00:12:12 +0300

    AsynchronousTickTock.csproj

---
+++ DotNetTricks/02. AsynchronousTickTockAsStateMachine/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.AsynchronousTickTockAsStateMachine
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TickTockAsync();
+
+            Console.ReadKey();
+        }
+
+        private static void TickTockAsync()
+        {
+            var stateMachine = new TickTockAsyncStateMachine();
+
+            stateMachine.MoveNext();
+
+        }
+
+        public class TickTockAsyncStateMachine
+        {
+            private int state = 0;
+            private TaskAwaiter awaiter;
+            public void MoveNext()
+            {
+                switch (state)
+                {
+                    case 0: goto firstState;
+                    case 1: goto secondState;
+                    case 2: goto thirdState;
+                }
+
+            firstState:
+                Console.WriteLine("Starting clock");
+                goto secondAndHalfState;
+
+            secondState:
+               awaiter.GetResult();
+
+            secondAndHalfState:
+                Console.Write("Tick");
+                awaiter = Task.Delay(500).GetAwaiter();
+                if (!awaiter.IsCompleted)
+                {
+                    state = 2;
+                    awaiter.OnCompleted(MoveNext);
+                    return;
+                }
+
+            thirdState:
+                awaiter.GetResult();
+                Console.WriteLine("Tock");
+                if (!awaiter.IsCompleted)
+                {
+                    state = 1;
+                    awaiter.OnCompleted(MoveNext);
+                    return;
+                }
+                goto secondState;
+            }
+        }
+    }
+}
---
+++ DotNetTricks/AsynchronousTickTock/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousTickTock
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TickTockAsync();
+
+            //Console.ReadKey();
+        }
+
+        private static async void TickTockAsync()
+        {
+            Console.WriteLine("Starting Clock");
+            while (true)
+            {
+                Console.Write("Tick ");
+                await Task.Delay(500);
+                Console.WriteLine("Tock");
+                await Task.Delay(500);
+            }
+        }
+    }
+}
