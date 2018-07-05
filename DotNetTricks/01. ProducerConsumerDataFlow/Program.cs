5982c2224595b783a6236eaa881d7578220fb971
rusrc <ruslan878707@gmail.com>
2018-07-05 21:21:43 +0300

    Commit

---
+++ DotNetTricks/01. ProducerConsumerDataFlow/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace _01.ProducerConsumerDataFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var consumerBlock = new ActionBlock<int>(new Action<int>(Consume));
+            PrintThreadPoolUsage("Main");
+            for (int i = 0; i < 5; i++)
+            {
+                consumerBlock.Post(i);
+                Thread.Sleep(1000);
+                PrintThreadPoolUsage("loop");
+            }
+            // Tell the block no more items will be coming
+            consumerBlock.Complete();
+            // wait for the block to shutdown
+            consumerBlock.Completion.Wait();           
+        }
+
+        private static void Consume(int val)
+        {
+            PrintThreadPoolUsage("Consume");
+            Console.WriteLine("{0}:{1} is thread pool thread {2}", Task.CurrentId, val,
+            Thread.CurrentThread.IsThreadPoolThread);
+        }
+
+        private static void PrintThreadPoolUsage(string label)
+        {
+            int cpu;
+            int io;
+            ThreadPool.GetAvailableThreads(out cpu, out io);
+            Console.WriteLine("{0}:CPU:{1},IO:{2}", label, cpu, io);
+        }
+    }
+}
---
+++ DotNetTricks/TaskCompletionSourceMoreEfficientUse/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskCompletionSourceMoreEfficientUse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        
+        static Task SleepAsyncA(int timeout)
+        {
+            return Task.Factory.StartNew(() => Thread.Sleep(timeout));
+        }
+
+        static Task SleepAsyncB(int timeout)
+        {
+            TaskCompletionSource<bool> tcs = null;
+            var t = new System.Threading.Timer((data) => tcs.TrySetResult(true) , null, -1, -1);
+            tcs = new TaskCompletionSource<bool>(t);
+            t.Change(timeout, -1);
+
+            return tcs.Task;
+        }
+    }
+}
